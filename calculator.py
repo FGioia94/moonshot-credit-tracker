77899e17cd504b44dc023d9872ccd12fff40a232
FGioia94 <info@francescogioia.it>
2026-02-23 15:43:52 +0100

    ready for a simple ui

--- calculator.py
+++ calculator.py
@@ -1,27 +1,32 @@
 import json
 
-with open("assets_data.json", "w") as f:
-    ASSET_DATA = json.read(f)
+with open("assets_data.json", "r") as f:
+    ASSET_DATA = json.load(f)
 
 class Calculator():
 
     def __init__(self):
         self.asset_purchases = {}
         
-    def add_asset_purchase(self, asset, amount):
-        self.asset_purchases[asset] = amount
-
-    def add_all_assets_purchase(self, amount):
-        for asset in self.asset_purchases:
-            self.asset_purchases[asset] += amount
+    def add_asset_purchase(self, asset, amount, price_without_vat):
+        price_without_vat *= 0.6 # removing the studio/marketing part
+        if not asset in self.asset_purchases:
+            self.asset_purchases[asset] = 0
+        self.asset_purchases[asset] += (price_without_vat * amount)
 
     def calculate(self):
         for asset in self.asset_purchases:
+            total_money = self.asset_purchases[asset]
             total_credits = 0
             for artist in ASSET_DATA[asset]:
                 total_credits += ASSET_DATA[asset][artist]
 
             for artist in ASSET_DATA[asset]:
-                percentage = (ASSET_DATA[asset][artist]/total_credits)*100
-
+                percentage = (ASSET_DATA[asset][artist]/total_credits)
+                revenue = total_money * percentage
+                print(f"artist> {artist}, asset > {asset}, perc > {percentage}, revenue > {revenue}\n")
                 
+                
+calc = Calculator()
+calc.add_asset_purchase("Rhino", 3, 4000)
+calc.calculate()
